4d367bddb91195999c55c50274f6d63ab0e5d07b
Svetlana Sokolova <Svetlana.Sokolova@gmail.com>
2018-06-27 10:45:00 -0700

    updating

--- app.js
+++ app.js
@@ -6,27 +6,27 @@ function clickQuest () {
     console.log(quest1Display);
 }*/
 var defaultQuestions = [
-    question1 = {
+{
     question: "Where are the three smallest bones in the human body?", 
     answers: ["middle ear", "nose", "toes", "eyes"],
     correctAnswer: answers[0]
 },
-    question2 = {
+{
     question: "What is the most abundant element in the Universe?",
     answers: ["Helium", "Oxygen", "Lithium", "Hydrogen"],
     correctAnswer: answers[3]
 },
-    question3 = {
+{
     question: "Approximately how long does it take for light to travel from the Sun's surface to the Earth?",
     answers: ["8 days", "8 seconds", "8 minutes", "8 hours"],
     correctAnswer: answers[2]
 },
-    question4 = {
+{
     question: "What is 10/2?",
     answers: ["5", "2", "8", "9"],
     correctAnswer: answers[0]
 },
-    question5 = {
+{
     question: "Which planet has the most moons?",
     answers: ["Saturn", "Mars", "Jupiter", "Uranus"],
     correctAnswer: answers[2]
