4e81e38ad717c7b7999c56a543c4633b2ef0dc00
Svetlana Sokolova <Svetlana.Sokolova@gmail.com>
2018-06-27 13:21:42 -0700

    updated js file

--- app.js
+++ app.js
@@ -9,25 +9,30 @@ var defaultQuestions = [
 {
     question: "Where are the three smallest bones in the human body?", 
     answers: ["middle ear", "nose", "toes", "eyes"],
-    correctAnswer: answers[0]
+    correctAnswer: 0
 },
 {
     question: "What is the most abundant element in the Universe?",
     answers: ["Helium", "Oxygen", "Lithium", "Hydrogen"],
-    correctAnswer: answers[3]
+    correctAnswer: 3
 },
 {
     question: "Approximately how long does it take for light to travel from the Sun's surface to the Earth?",
     answers: ["8 days", "8 seconds", "8 minutes", "8 hours"],
-    correctAnswer: answers[2]
+    correctAnswer: 2
 },
 {
     question: "What is 10/2?",
     answers: ["5", "2", "8", "9"],
-    correctAnswer: answers[0]
+    correctAnswer: 0
 },
 {
     question: "Which planet has the most moons?",
     answers: ["Saturn", "Mars", "Jupiter", "Uranus"],
-    correctAnswer: answers[2]
+    correctAnswer: 2
 }];
+var currentQuestion = 0;
+function populateQuestion() {
+    $("quizStatus").html("Question" + currentQuestion + " out of" + defaultQuestions.length);
+
+}
